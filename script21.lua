b2ed64f14260c07aa8b58655af436dfd1c83eb92
pavelk777 <39745256+pavelk777@users.noreply.github.com>
2018-05-30 21:50:12 +0200

    Add files via upload

---
+++ script21.lua
@@ -0,0 +1,22 @@
+local ArmletAbuser = {}
+
+ArmletAbuser.optionEnable = Menu.AddOptionBool({ "Utility" }, "ArmletAbuser")
+
+function ArmletAbuser.OnUpdate()
+    
+	if not Menu.IsEnabled(ArmletAbuser.optionEnable) then return end
+	local myHero = Heroes.GetLocal()
+	local Armlet = NPC.GetItem(myHero, "item_armlet", true)
+	
+	
+	if NPC.HasModifier(myHero, Entity.GetHealth(myHero.Hero) = 200) and Ability.IsReady(Armlet) then
+		    local hero = Heroes.GetLocal()
+		    Ability.CastNoTarget(armlet, false)
+		    Ability.CastNoTarget(armlet, false)
+
+			end 
+		end
+	end
+end
+
+return ArmletAbuser
